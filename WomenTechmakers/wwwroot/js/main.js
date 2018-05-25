1c31bb6ae2a70e4103cb3110239b7bc8a6e612bb
Mary Camila <boty184@hotmail.com>
2018-05-24 22:42:08 -0300

    Update in sections

--- WomenTechmakers/wwwroot/js/main.js
+++ WomenTechmakers/wwwroot/js/main.js
@@ -483,7 +483,21 @@
         // Animations
         homeAnimate();
         introAnimate();
-        typed();
+
+        // device detection
+        if (navigator.userAgent.match(/Android/i)
+		 || navigator.userAgent.match(/webOS/i)
+		 || navigator.userAgent.match(/iPhone/i)
+		 || navigator.userAgent.match(/iPad/i)
+		 || navigator.userAgent.match(/iPod/i)
+		 || navigator.userAgent.match(/BlackBerry/i)
+		 || navigator.userAgent.match(/Windows Phone/i)
+		 ) {
+            $("#text-header").html("WTM Curitiba");
+        } else {
+            typed();
+        }
+
         workAnimate();
         testimonialAnimate();
         servicesAnimate();
